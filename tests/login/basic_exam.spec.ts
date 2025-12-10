3f474c8175a76d3e4deacfa01f49e5b8c627fd7f
nguyen_thi_hue-c+1-at-905418243710 <nguyen.thi.hue-c@sun-asterisk.com>
2025-12-10 16:25:03 +0700

    Task: Login with saucedemo - basic html exam

---
+++ tests/login/basic_exam.spec.ts
@@ -0,0 +1,11 @@
+/**
+ * @fileoverview Basic HTML exam test cases.
+ - Use page: https://www.saucedemo.com/
+ - Create 4 test cases: 
+ + Login success with: standard_user + secret_sauce -> open page product 
+ + Login failed, wrong password -> showing error message
+ + Login failed, not fill username -> showing error message
+ + Login with locked_out_user -> showing error message
+
+ => Create 2 files: login-valid.spec.js, login-invalid.spec.js inside folder login
+ */
